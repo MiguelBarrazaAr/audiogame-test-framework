a813c7fc6bc3fe736aae0cf6715eb2cd4a2d5e89
Miguel Barraza <miguelbarraza2015@gmail.com>
2018-10-31 18:18:21 -0300

    agregado el setup para poder instalar la libreria

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# iku-engine
-iku engine motor de creaciÃ³n de videojuegos desarrollado en python: basado en pygame.
+# iku: engine de juegos accesibles
+
+  Iku, motor de creación de videojuegos desarrollado en python 3.7.
+Basado en pygame.
+
+##Objetivos:
+
+  Iku es un motor de juegos 2d (game engine) desarrollado para facilitar la creación de audiojuegos o videojuegos accesibles.
+Contiene toda las librerías necesarias para el manejo con lectores de pantalla y la manipulación de imagenes en 2d y audio en 2d y 3d.
+nota: en face desarrollo aún.
+
+##Requiere las siguientes librerías:
+- pygame 1.9.4
+- libaudioverse 0.9.1
+- accessible-output2
+- sound-lib 0.8
+
+##Como instalarlo:
+
+  Se recomienda clonar el repo en tu disco, y luego instalarlo en modo develop con el siguiente comando:
+
+python setup.py develop
+
+de esa forma al  actualizar el repo git con (git pull) tendrás todos los nuevos cambios.
---
+++ setup.py
@@ -0,0 +1,40 @@
+﻿#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import os
+import sys
+from setuptools import setup
+
+setup(
+        name='iku',
+        zip_safe=False,
+        version="0.1",
+        description="""Iku Engine - un motor para realizar videojuegos y audiojuegos de manera rápida y sencilla.
+
+Es una herramienta orientada a desarrollar juegos accesibles para personas con discapacidad visual.
+""",
+        author='Miguel Barraza',
+        author_email='miguelbarraza2015@gmail.com',
+        install_requires=[
+            'setuptools',
+            'pygame',
+            'libaudioverse',
+            'accessible-output2',
+            ],
+        packages=[
+            'iku',
+            'iku.actores',
+            'iku.complementos',
+            'iku.escenas',
+            'iku.eventos',
+            'iku.sonido',
+            'iku.utiles',
+        ],
+        url='http://www.iku-engine.com.ar',
+        include_package_data = True,
+
+        package_data = {
+            'images': [ 'iku/imagenes/*' ]
+        },
+
+        scripts=[]#['bin/iku'],
+)
