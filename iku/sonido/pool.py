76e4ec858c04faedc6a7d67ec13c7dee1dd0f876
Miguel Barraza <miguelbarraza2015@gmail.com>
2019-01-05 09:50:02 -0300

    se agrega parametros opcionales al callback de la reproduccion del pool

--- iku/sonido/pool.py
+++ iku/sonido/pool.py
@@ -12,7 +12,7 @@ class SoundPool(object):
     self.x = 0
     self.lista = [None]*cantidadDeSonidos
   
-  def reproducir(self, ruta, posicion=(0,0,0), respuesta=None, espera=0):
+  def reproducir(self, ruta, posicion=(0,0,0), respuesta=None, espera=0, *args, **kwargs):
     """carga y reproduce un sonido"""
     px, py, pz = posicion
     pos = (px-self.camx, py-self.camy, pz-self.camz)
@@ -22,8 +22,7 @@ class SoundPool(object):
     self.x = (self.x+1)%self.cantidadDeSonidos
     # si tiene callback agregamos una tarea:
     if respuesta is not None:
-      self.iku.tareas.condicional(2, FinalizaSonido(self.iku, sonido, respuesta, espera))
-      #self.iku.tareas.unaVez(sonido.duracion+espera, respuesta)
+      self.iku.tareas.condicional(0.1, FinalizaSonido(self.iku, sonido, respuesta, espera, *args, **kwargs))
     return sonido
   
   def _alMoverCamara(self, evento):
@@ -33,15 +32,17 @@ class SoundPool(object):
     self.camx, self.camy, self.camz = self.iku.camara.posicion
 
 class FinalizaSonido():
-  def __init__(self, iku, sonido, respuesta, espera):
+  def __init__(self, iku, sonido, respuesta, espera, *args, **kwargs):
     self.iku = iku
     self.sonido = sonido
     self.espera = espera
     self.respuesta = respuesta
+    self.args=args
+    self.kwargs = kwargs
   
   def __call__(self):
     if self.sonido.duracion == self.sonido.transcurrido():
-      self.iku.tareas.unaVez(self.espera, self.respuesta)
+      self.iku.tareas.unaVez(self.espera, self.respuesta, *self.args, **self.kwargs)
       return False
     else:
       return True
