b092d624aafe9813e5b562df8d9160b102622ccb
Miguel Barraza <miguelbarraza2015@gmail.com>
2019-07-26 22:08:45 -0300

    si implementa una funcion para mover un punto segun el cardinal determinado

--- iku/utiles/cardinal.py
+++ iku/utiles/cardinal.py
@@ -8,21 +8,37 @@ from enum import IntEnum
 
 class Cardinal(IntEnum):
   norte=0
-  n=0
   noreste=1
-  ne=1
   este=2
-  e=2
   sureste=3
-  se=3
   sur=4
-  s=4
   suroeste=5
-  so=5
   oeste=6
-  o=6
   noroeste=7
-  no=7
 
 def invertir(coord):
   return Cardinal((coord.value+4)%8)
+
+def mover(posicion, cardinal, pasos=1):
+  x, y = posicion
+  if cardinal.name == 'norte':
+    y+=pasos
+  elif cardinal.name == 'sur':
+    y-=pasos
+  elif cardinal.name == 'este':
+    x+=pasos
+  elif cardinal.name == 'oeste':
+    x-=pasos
+  if cardinal.name == 'noreste':
+    y+=pasos
+    x+=pasos
+  if cardinal.name == 'noroeste':
+    y+=pasos
+    x-=pasos
+  if cardinal.name == 'sureste':
+    y-=pasos
+    x+=pasos
+  if cardinal.name == 'suroeste':
+    y-=pasos
+    x-=pasos
+  return (x, y)
